4b4d1400eb9535b6774a122fe655c52e297af295
小原正大 <shota@shota-MacBook-Air.local>
2014-10-21 02:35:00 +0900

    making

--- practice/integrate.c
+++ practice/integrate.c
@@ -1,41 +1,89 @@
+//シンプソン公式利用
+//http://www.opt.utsunomiya-u.ac.jp/~yatagai/Lectures/jouhou/Chap6.htm
+
 #include <stdio.h>
-//シンプソン公式を利用
-//観測データを先ず読み込む
-//一秒分のポイント数(N)を定義してその分だけの要素数をもった観測強度の配列を作成
-#define N 100
-double func(double x)
-{
-  return(1.0/x);
-}
-//作成された配列の分だけループをまわす
-//[["21","231","12", ..Nの数だけ.. , "1224"], ... , ["", ... , ""] ]
+#include <math.h>
+
 int main(void)
 {
-  int i;
-  double a,b,s,s1,s2;
-  double x,y[N+1],d;
-  //a, bは必要ないね。xも必要ない
-  //dに(観測秒/観測ポイント) = (1/N) を入れたら解決？ 
-  a=1,0; b=2.0;
+  int i, j, ret;
+  float tmp;
+  FILE *fp;
+  char *inputfname = "data.csv";
+  char *outputfname = "integrate.csv";
+  
+  //シンプソン公式のためのsを定義
+  double s,s1,s2;
+  //初期化
   s=0.0; s1=0.0; s2=0.0;
-  d=(b-a)/(double)N;
-  for(i=0; i<=N; i++)
-  {
-    x=(double)i*d+a;
-    //yに観測データの配列からとってきた観測強度を入れる
-    y[i]=func(x);
-  }
-  for(i=1; i<=N-1;i+=2)
-  {
-    s1+=y[i];
-  }
-  for(i=2;i<=N-2;i+=2)
-  {
-    s2+=y[i];
-  }
-  s=(y[0]+4.0*s1+2.0*s2+y[N])*d/3.0;
+  
+  //何秒積分か入力
+  double sec;
+  //1秒あたりの観測ポイント数を定義(1ポイント毎の秒数としても利用)
+  double n;
+  //観測合計時間
+  double len;
+  printf("何秒積分したいですか？(double): ");
+  scanf("%lf",&sec);
+  printf("毎秒何ポイント観測してますか？(double): ");
+  scanf("%lf",&n);
+  printf("何秒観測してますか？(double): ");
+  scanf("%lf",&len);
+  printf("test"); 
+  
+  //積分の配列に入れるポイント数を定義
+  int p = ceil(sec*n);
+  //合計ポイント数を定義
+  int N = ceil(len*n);
+  //ループ処理回数
+  int loop = floor(N/p);
+  //配列を定義。p個分観測データを入れる
+  double y[p+1];
+  double data[N+1];
 
-  printf("Intergral : %f\n",s);
+  //読み込みファイルを開く
+  fp = fopen( inputfname, "r" );
+  if( fp == NULL ){
+    printf( "%sファイルが開けません¥n", inputfname );
+    return -1;
+  }
+  i = 0;
+  while(( ret = fscanf( fp, "%f", &tmp )) != EOF ){
+    data[i] = tmp;
+    i = i + 1;
+  }
+  
+  //書き込みファイルを開く
+  fp = fopen( outputfname, "w" );
+  if( fp == NULL ){
+    printf( "%sファイルが開けません\n", outputfname );
+    return -1;
+  }
+  
+  for(j=0; j<=loop; j++){
+    //この辺はシンプソン公式通り
+    for(i=0; i<=p; i++)
+    {
+      y[i]=data[i+(p+1)*j];
+    }
+    for(i=1; i<=p-1;i+=2)
+    {
+      s1+=y[i];
+    }
+    for(i=2;i<=p-2;i+=2)
+    {
+      s2+=y[i];
+    }
+    s=(y[0]+4.0*s1+2.0*s2+y[p])/n/3.0;
+    
+    printf("Intergral : %f\n",s);
+    
+    //csvファイルに書き込み
+    fprintf( fp, "%f\n", s);
+  }
+  //編集終了
+  fclose( fp );
+  printf( "%sファイル書き込みが終わりました\n", outputfname );
+  return 0;
 }
 
-//作成した1秒積分の配列を一行ずつprintfして、それをグラフ化する
---
+++ practice/scanf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+  int a;
+  printf("数字を入力して下さい：");
+  scanf("%d",&a);
+  printf("入力された数値%dはです\n",a);
+  return 0;
+}
